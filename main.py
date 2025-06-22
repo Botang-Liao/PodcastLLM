80c31a8e4c5a5727d6f990a15735601199060693
Botang <xt21109142@gmail.com>
2025-06-22 22:35:40 +0800

    modification on vdb path

--- main.py
+++ main.py
@@ -119,7 +119,7 @@ def setup_qa_chain(use_cpu=False):
     memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
     
     # 使用新的FAISS檢索邏輯，並傳入 use_cpu 參數
-    parent_directory = r"/home/sylvia2004/projects/PodcastLLM/VectoreStore_sample"  # 更新為您的向量庫目錄
+    parent_directory = r"/home/sylvia2004/projects/PodcastLLM/vector_store"  # 更新為您的向量庫目錄
     embeddings = create_embeddings(use_cpu)
     vectorstores = load_vectorstores_from_directory(parent_directory, embeddings)
     
